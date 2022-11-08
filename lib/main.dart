787eb4043da9dab3dbcf300aea14bc940900fc44
DipeanDas <dipeandas15033@gmail.com>
2022-11-08 21:39:14 +0530

    Day3

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_prac1/utils/routes.dart';
 
+import 'package:google_fonts/google_fonts.dart';
 import 'Pages/home_page.dart';
 import 'Pages/login_page.dart';
 
@@ -13,13 +15,19 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // ignore: prefer_const_constructors
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        initialRoute: "/home",
-        routes: {
-          "/": (context) => LoginPage(),
-          "/home": (context) => HomePage(),
-          "/login": (context) => LoginPage(),
-        });
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+          primarySwatch: Colors.deepPurple,
+          primaryTextTheme: GoogleFonts.latoTextTheme()),
+      debugShowCheckedModeBanner: false,
+      initialRoute: MyRoutes.loginRoute,
+      routes: {
+        "/": (context) => LoginPage(),
+        MyRoutes.homeRoute: (context) => HomePage(),
+        MyRoutes.loginRoute: (context) => LoginPage(),
+      },
+    );
   }
 }
